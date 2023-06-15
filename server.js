4b9fba5f6e06ada6b4bece5f7e505c2ed5bc41d1
div <div.hung@gmail.com>
2023-06-15 23:21:20 +0700

    update travis and server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-
+// Set a local variable populated by the environment variable $NAME
+const FAVOURITE_INSTRUCTOR = process.env.NAME;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`${FAVOURITE_INSTRUCTOR} rocks`);
     await sleep(5000);
   }
 }
